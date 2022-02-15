ed7eebcce5815c44447b6e68dd845950cc1e6bbd
samuel <samuel.borges@maida.health>
2022-02-14 23:00:21 -0300

    Adding device gem

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,5 +1,6 @@
 class AnimesController < ApplicationController
   before_action :set_anime, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index]
 
   # GET /animes or /animes.json
   def index
@@ -8,6 +9,7 @@ class AnimesController < ApplicationController
 
   # GET /animes/1 or /animes/1.json
   def show
+    
   end
 
   # GET /animes/new
