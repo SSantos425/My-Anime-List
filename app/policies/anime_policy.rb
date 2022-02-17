3a753e1bd0421cc856bf81dc42284b69efe01cf6
samuel <samuel.borges@maida.health>
2022-02-16 23:44:44 -0300

    Adding partials for admin and otaku

--- app/policies/anime_policy.rb
+++ app/policies/anime_policy.rb
@@ -2,12 +2,10 @@ class AnimePolicy < ApplicationPolicy
   def index?
     user.admin?      
   end
-  class Scope < Scope
 
-    
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
   end
 end
