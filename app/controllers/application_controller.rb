caec69de6c8c7ac7793c3c1c588eda9f584bc83e
samuel <samuel.borges@maida.health>
2022-02-16 21:08:59 -0300

    entering pundit and adm and otaku privileges

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+include Pundit
+protect_from_forgery with: :exception
+
+rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+private
+    def user_not_authorized
+        flash[:notice] = "you do not have permission to perfome this action"
+        redirect_to(request.referrer||root_path)
+    end
 end
